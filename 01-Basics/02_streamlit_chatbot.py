6d5a38308e4e7aacd5cc18285f289f9808b7fdd1
vijaynvb <vijay.nvb@gmail.com>
2025-07-24 12:01:12 +0530

    git ignore

--- 01-Basics/02_streamlit_chatbot.py
+++ 01-Basics/02_streamlit_chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Company Chatbot",
+    page_title="Carelon Chatbot",
     page_icon=":robot_face:",
     layout="centered",
     initial_sidebar_state='expanded',
